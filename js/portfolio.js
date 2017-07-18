1250546589f165e2182295187557c053240858c8
biancamika <biancamika128@gmail.com>
2017-07-18 11:28:24 +0800

    try to fix imageload

---
+++ js/portfolio.js
@@ -0,0 +1,23 @@
+/*
+$lg.lightGallery();
+  $lg.append(slide);
+  $lg.data('lightGallery').destroy(true);
+  $lg.lightGallery();
+
+*/
+
+// external js: isotope.pkgd.js
+
+// init Isotope
+$('.grid').isotope({
+// options
+itemSelector: '.grid-item',
+ percentPosition: true,
+ masonry: {
+ fitWidth: true
+  }
+  });
+ $('a').on( 'click', function() {
+ var filterValue = $(this).attr('data-filter');
+  $('.grid').isotope({ filter: filterValue });
+  });
